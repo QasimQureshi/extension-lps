144afae50fef823a97cf87522a7e41109575b2e8
Qasim Qureshi <qasim.qureshi@gmail.com>
2019-05-14 15:52:34 +0500

    Triggering CWS modal using script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,55 +1,6 @@
-//downloadBtnClassName: "downloadButton", // The CSS class of the main "Download" button and "No" button of the "Are you sure you want to cancel installation" dialog.
-//    cancelDownloadBtnClassName: "confirmNoBtn", //
-
-/* Offsite Script Configuration */
-  (function(w){
-    window._dlpOffsite = {
-    type: "CHROME_CWS_DIMMED",
-    dsFirstFlow: false,
-    ttDetectNTAutoRedirect: true,
-    log: false,
-    downloadBtnClassName: "downloadButton", // The CSS class of the main "Download" button and "No" button of the "Are you sure you want to cancel installation" dialog.
-    cancelDownloadBtnClassName: "confirmNoBtn", // The unique CSS class of the "Yes" button of the "Are you sure you want to cancel installation" dialog.
-    shouldOpenNewTabDS: true,
-    loader: {
-        domain: "creativeinternetds.dl.mysearch.com",
-        campaign: "",
-        cobrand: "",
-        sub_id: "",//"PARTNER_SPECIFIC_VALUES_123",
-        beforeload: function () {
-        },
-        onload: function () {
-        }
-    },
-    handlers: {
-        SPLASH_LANDING: function (data) {
-            //console.log('offsite handler for LP - fire splash pixels or something');
-        },
-        SPLASH_LANDING_CLICKED: function (data) {
-            //console.log('offsite handler for LP - fire splash pixels or something');
-        },
-        INSTALLER_FINISHED: function (data) {
-            //console.log('First offer extension has been installed successfully');
-        },
-        TOOLTAB_DETECT: function (data) {
-            //console.log('offsite handler for tooltab detect');
-            if (data.present) {
-                console.log('Extension already installed - redirect to success page');
-                window.location = data.newTabURL;
-            }
-        },
-        ERROR: function (data) {
-            if (data.errorCode == "User cancelled install") {
-                diag.toggle();
-            }
-        }
-    }
-  }
-})(window);
-
 document.addEventListener('DOMContentLoaded', (event) => {
   
-  // Download button click-handler
+  // Download button click-handler (blurs the background, plays the audio-clip)
   document.querySelector('.action-buttons > .downloadButton').addEventListener('click', (event) => {
     
     document.querySelector('body').classList.add('overlay'); // adds blur
@@ -63,7 +14,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     var audio = new Audio('assets/audio/7204.mp3');
     audio.play();
     
-    showPopup();
   })
   
   // Cancel confirmation, yes button (don't re-open popup)
@@ -74,40 +24,83 @@ document.addEventListener('DOMContentLoaded', (event) => {
   // Cancel confirmation, no button (re-open popup)
   document.querySelector('.cancel-confirmation-card .action-buttons > .confirmNoBtn').addEventListener('click', (event) => {
     document.querySelector('body').classList.remove('cancel-confirmation');
-    showPopup();
+    document.querySelector('.action-buttons > .downloadButton').click(); // trigger a click event on the button
   })
   
 })
 
-// Opening a centered popup
-function showPopup () {
-  
-  var url = 'https://chrome.google.com/webstore/detail/mysearch-ds-4/nlamdhlghmeopdnpagckfgjklpkmaggl',
-      w = 927,
-      h = 590,
-      title = "MySearch DS 4";
-  // Fixes dual-screen position                         Most browsers      Firefox
-  var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
-  var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
 
-  width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
-  height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
 
-  var left = ((width / 2) - (w / 2)) + dualScreenLeft;
-  var top = ((height / 2) - (h / 2)) + dualScreenTop;
-  var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+/* Offsite Script Configuration */
+(function(w) {
+    var diag = new confirmationDialog();
+    window.diag = diag;
 
-  // Puts focus on the newWindow
-  if (window.focus) {
-      newWindow.focus();
-  }
-  
-  // Checking if this window was closed
-  var windowCloseTick = setInterval(function(){
-    if(newWindow.closed) {
-      clearInterval(windowCloseTick);
-      
-      document.querySelector('body').classList.add('cancel-confirmation');
+    w._dlpOffsite = {
+        type: "CHROME_CWS_DIMMED",
+        dsFirstFlow: false,
+        ttDetectNTAutoRedirect: true,
+        log: false,
+        downloadBtnClassName: "downloadButton", // The CSS class of the main "Download" button and "No" button of the "Are you sure you want to cancel installation" dialog.
+        cancelDownloadBtnClassName: "confirmNoBtn", // The unique CSS class of the "Yes" button of the "Are you sure you want to cancel installation" dialog.
+        shouldOpenNewTabDS: true,
+        loader: {
+            domain: "creativeinternetds.dl.mysearch.com",
+            campaign: "",
+            cobrand: "",
+            sub_id: "",//"PARTNER_SPECIFIC_VALUES_123",
+            beforeload: function () {
+            },
+            onload: function () {
+            }
+        },
+        handlers: {
+            SPLASH_LANDING: function (data) {
+                //console.log('offsite handler for LP - fire splash pixels or something');
+            },
+            SPLASH_LANDING_CLICKED: function (data) {
+                //console.log('offsite handler for LP - fire splash pixels or something');
+            },
+            INSTALLER_FINISHED: function (data) {
+                //console.log('First offer extension has been installed successfully');
+            },
+            TOOLTAB_DETECT: function (data) {
+                //console.log('offsite handler for tooltab detect');
+                if (data.present) {
+                    console.log('Extension already installed - redirect to success page');
+                    window.location = data.newTabURL;
+                }
+            },
+            ERROR: function (data) {
+                if (data.errorCode == "User cancelled install") {
+//                    diag.toggle();
+                }
+            }
+        }
+    };
+
+    function confirmationDialog() {
+//        this.dialog = document.getElementById("confirmDialog"),
+//        this.backdrop = document.getElementById("modal-backdrop");
+//        this.yes_btn = document.querySelector("#confirmDialog .btn-yes");
+//        this.no_btn = document.querySelector("#confirmDialog .btn-no");
+
+//        var self = this;
+//
+//        this.yes_btn.addEventListener('click', function(e) {
+//            self.toggle();
+//        });
+//        this.no_btn.addEventListener('click', function(e) {
+//            self.toggle();
+//        });
     }
-  })
-}
+
+    confirmationDialog.prototype.toggle = function() {
+      console.log('toggle is called');
+//        this.backdrop.classList.toggle("hide");
+//        this.backdrop.classList.toggle("show");
+//        this.dialog.classList.toggle("hide");
+//        this.dialog.classList.toggle("show");
+    };
+})(window);
+// showConfirmationDialog();
