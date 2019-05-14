8054eff9f6b41215844ae2280cc627d7cfb5aa4b
Qasim Qureshi <qasim.qureshi@gmail.com>
2019-05-14 09:30:45 +0500

    Added DLP script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,52 @@
+//downloadBtnClassName: "downloadButton", // The CSS class of the main "Download" button and "No" button of the "Are you sure you want to cancel installation" dialog.
+//    cancelDownloadBtnClassName: "confirmNoBtn", //
+
+/* Offsite Script Configuration */
+  (function(w){
+    window._dlpOffsite = {
+    type: "CHROME_CWS_DIMMED",
+    dsFirstFlow: false,
+    ttDetectNTAutoRedirect: true,
+    log: false,
+    downloadBtnClassName: "downloadButton", // The CSS class of the main "Download" button and "No" button of the "Are you sure you want to cancel installation" dialog.
+    cancelDownloadBtnClassName: "confirmNoBtn", // The unique CSS class of the "Yes" button of the "Are you sure you want to cancel installation" dialog.
+    shouldOpenNewTabDS: true,
+    loader: {
+        domain: "creativeinternetds.dl.mysearch.com",
+        campaign: "",
+        cobrand: "",
+        sub_id: "",//"PARTNER_SPECIFIC_VALUES_123",
+        beforeload: function () {
+        },
+        onload: function () {
+        }
+    },
+    handlers: {
+        SPLASH_LANDING: function (data) {
+            //console.log('offsite handler for LP - fire splash pixels or something');
+        },
+        SPLASH_LANDING_CLICKED: function (data) {
+            //console.log('offsite handler for LP - fire splash pixels or something');
+        },
+        INSTALLER_FINISHED: function (data) {
+            //console.log('First offer extension has been installed successfully');
+        },
+        TOOLTAB_DETECT: function (data) {
+            //console.log('offsite handler for tooltab detect');
+            if (data.present) {
+                console.log('Extension already installed - redirect to success page');
+                window.location = data.newTabURL;
+            }
+        },
+        ERROR: function (data) {
+            if (data.errorCode == "User cancelled install") {
+                diag.toggle();
+            }
+        }
+    }
+  }
+})(window);
+
 document.addEventListener('DOMContentLoaded', (event) => {
   
   // Download button click-handler
